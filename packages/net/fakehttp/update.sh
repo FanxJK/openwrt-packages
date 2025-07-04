85a42f62532a5635daf4f394e8f6e973571e716a
Fanx <fanxjk@gmail.com>
2025-07-04 10:26:04 +0800

    initialization

--- packages/net/fakehttp/update.sh
+++ packages/net/fakehttp/update.sh
@@ -7,7 +7,7 @@ export CURDIR="$(cd "$(dirname $0)"; pwd)"
 curl -fsSL "https://api.github.com/repos/MikeWang000000/FakeHTTP/releases/latest" -o "$CURDIR/latest.json" || exit 1
 VERSION="$(jq -r ".tag_name" "$CURDIR/latest.json" || exit 1)"
 PKG_VERSION="$(awk -F "PKG_VERSION:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
-[ "$PKG_VERSION" != "$VERSION" ] || exit 0
+[ "$PKG_VERSION" != "$VERSION" ] || rm -f "$CURDIR/latest.json" ||exit 0
 
 
 sed -i "s,PKG_VERSION:=.*,PKG_VERSION:=$VERSION,g" "$CURDIR/Makefile"
@@ -16,11 +16,9 @@ for ARCH in "x86_64"; do
     if [ "$ARCH" = "x86_64" ]; then
         FILE_HASH=$(jq -r '.assets[] | select(.name=="fakehttp-linux-x86_64.tar.gz") | .digest' "$CURDIR/latest.json" | sed 's/^sha256://')
     fi
-    
+
     HASH_LINE="$(($(sed -n -e "/(\$(ARCH),$ARCH)/=" "$CURDIR/Makefile") + 1))"
     sed -i "${HASH_LINE}s/PKG_HASH:=.*/PKG_HASH:=$FILE_HASH/" "$CURDIR/Makefile"
 done
-
-sed -i "s,PKG_HASH:=.*,PKG_HASH:=$PKG_HASH,g" "$CURDIR/Makefile"
 
 rm -f "$CURDIR/latest.json"
