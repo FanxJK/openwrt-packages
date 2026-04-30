02bf04a91f1d65329ace1b093757680a85e1d34f
Fanx <fanxjk@gmail.com>
2026-04-30 19:19:29 +0800

    Polish wolplus wake controls
    Separate one-time wake from saved device editing and fix the LuCI menu registration dependency.

--- luci/applications/luci-app-wolplus/htdocs/luci-static/resources/view/wolplus.js
+++ luci/applications/luci-app-wolplus/htdocs/luci-static/resources/view/wolplus.js
@@ -62,13 +62,17 @@ function addHostHints(option, hints) {
 	});
 }
 
+function optionValue(option, sectionId) {
+	var value = option.formvalue(sectionId);
+
+	if (!normalize(value))
+		value = uci.get('wolplus', sectionId, option.option);
+
+	return normalize(value);
+}
+
 function optionChanged(option, sectionId) {
-	var current = option.formvalue(sectionId);
-
-	if (current == null)
-		current = uci.get('wolplus', sectionId, option.option);
-
-	return normalize(current) !== normalize(uci.get('wolplus', sectionId, option.option));
+	return optionValue(option, sectionId) !== normalize(uci.get('wolplus', sectionId, option.option));
 }
 
 function hasUnsavedValues(options, sectionId) {
@@ -87,8 +91,46 @@ function isValidInterface(value) {
 	return value.length > 0 && value.charAt(0) !== '-' && /^[A-Za-z0-9_.:-]+$/.test(value);
 }
 
-function notify(message, severity) {
-	ui.addNotification(null, E('p', {}, message), severity || 'info');
+function showModal(title, message, output) {
+	var body = [ E('p', {}, message) ];
+
+	if (output)
+		body.push(E('pre', {
+			style: 'white-space: pre-wrap; max-height: 20em; overflow: auto;'
+		}, output));
+
+	body.push(E('div', { 'class': 'right' }, [
+		E('button', {
+			'class': 'btn cbi-button cbi-button-neutral',
+			click: ui.hideModal
+		}, _('Close'))
+	]));
+
+	ui.showModal(title, body);
+}
+
+function resolveClickArgs(args) {
+	var sectionId = null;
+	var button = null;
+
+	for (var i = 0; i < args.length; i++) {
+		if (typeof args[i] === 'string')
+			sectionId = args[i];
+		else if (args[i] && args[i].currentTarget)
+			button = args[i].currentTarget;
+		else if (args[i] && args[i].target)
+			button = args[i].target;
+	}
+
+	return [ sectionId, button ];
+}
+
+function disableButton(button, disabled) {
+	if (!button)
+		return;
+
+	button.disabled = disabled;
+	button.textContent = disabled ? _('Collecting data...') : _('awake');
 }
 
 return view.extend({
@@ -104,6 +146,69 @@ return view.extend({
 		var m = new form.Map('wolplus', _('wolplus'),
 			_('Wake up your LAN device') + '<br /><br />' +
 			'<a href="https://github.com/FanxJK/openwrt-packages/tree/main/luci/applications/luci-app-wolplus" target="_blank">Powered by sundaqiang, rewritten in JavaScript by Fanx.</a>');
+
+		var sendWake = function(mac, iface, button) {
+			if (!mac || !iface) {
+				showModal(_('Wake request failed'), _('Please configure MAC address and network interface first'));
+				return false;
+			}
+
+			if (!isValidMac(mac) || !isValidInterface(iface)) {
+				showModal(_('Wake request failed'), _('Invalid MAC address or network interface'));
+				return false;
+			}
+
+			disableButton(button, true);
+
+			return fs.exec('/usr/bin/etherwake', [ '-D', '-i', iface, '-b', mac ]).then(function(res) {
+				var output = commandOutput(res || {});
+
+				if (res != null && res.code != null && +res.code !== 0)
+					throw new Error(output || _('Wake request failed'));
+
+				showModal(_('Wake request sent'), output || _('Wake request sent'));
+			}).catch(function(err) {
+				showModal(_('Wake request failed'), err.message || String(err));
+			}).then(function() {
+				disableButton(button, false);
+			});
+		};
+
+		var once = m.section(form.TypedSection, '_wake_once', _('One-time wake'), _('Send a Wake-on-LAN packet without saving the device.'));
+		once.anonymous = true;
+		once.addremove = false;
+		once.cfgsections = function() {
+			return [ '_once' ];
+		};
+
+		var onceMac = once.option(form.Value, '_once_mac', _('macaddr'));
+		onceMac.rmempty = true;
+		onceMac.datatype = 'macaddr';
+		onceMac.cfgvalue = function() {
+			return '';
+		};
+		onceMac.write = function() {};
+		onceMac.remove = function() {};
+		addHostHints(onceMac, hostHints);
+
+		var onceIface = once.option(widgets.DeviceSelect, '_once_iface', _('maceth'));
+		onceIface.rmempty = true;
+		onceIface.nocreate = false;
+		onceIface.cfgvalue = function() {
+			return '';
+		};
+		onceIface.write = function() {};
+		onceIface.remove = function() {};
+
+		var onceWake = once.option(form.Button, '_once_awake', _('awake'));
+		onceWake.inputtitle = _('awake');
+		onceWake.inputstyle = 'apply';
+		onceWake.onclick = function() {
+			var args = resolveClickArgs(arguments);
+			return sendWake(optionValue(onceMac, '_once'), optionValue(onceIface, '_once'), args[1]);
+		};
+		onceWake.write = function() {};
+		onceWake.remove = function() {};
 
 		var s = m.section(form.GridSection, 'macclient', _('macclient'));
 		s.anonymous = true;
@@ -122,41 +227,26 @@ return view.extend({
 		oIface.rmempty = false;
 		oIface.nocreate = false;
 
-		var oWake = s.option(form.Button, '_awake', _('awake'));
-		oWake.inputtitle = _('awake');
-		oWake.inputstyle = 'apply';
-		oWake.onclick = function(arg1, arg2) {
-			var sectionId = (typeof arg1 === 'string') ? arg1 : arg2;
-
+		var wakeDevice = function(sectionId, button) {
 			if (!sectionId || hasUnsavedValues([ oName, oMac, oIface ], sectionId)) {
-				notify(_('Please [Save & Apply] your changes first'), 'warning');
+				showModal(_('Please [Save & Apply] your changes first'), _('Please [Save & Apply] your changes first'));
 				return false;
 			}
 
-			var mac = normalize(uci.get('wolplus', sectionId, 'macaddr'));
-			var iface = normalize(uci.get('wolplus', sectionId, 'maceth'));
-
-			if (!mac || !iface) {
-				notify(_('Please configure MAC address and network interface first'), 'warning');
-				return false;
-			}
-
-			if (!isValidMac(mac) || !isValidInterface(iface)) {
-				notify(_('Invalid MAC address or network interface'), 'warning');
-				return false;
-			}
-
-			return fs.exec('/usr/bin/etherwake', [ '-D', '-i', iface, '-b', mac ]).then(function(res) {
-				var output = commandOutput(res || {});
-
-				if (res != null && res.code != null && +res.code !== 0)
-					throw new Error(output || _('Wake request failed'));
-
-				notify(output || _('Wake request sent'), 'info');
-			}).catch(function(err) {
-				notify(_('Wake request failed') + ': ' + (err.message || err), 'danger');
-			});
+			return sendWake(optionValue(oMac, sectionId), optionValue(oIface, sectionId), button);
 		};
+
+		var oWakeList = s.option(form.Button, '_awake', _('awake'));
+		oWakeList.inputtitle = _('awake');
+		oWakeList.inputstyle = 'apply';
+		oWakeList.editable = true;
+		oWakeList.modalonly = false;
+		oWakeList.onclick = function() {
+			var args = resolveClickArgs(arguments);
+			return wakeDevice(args[0], args[1]);
+		};
+		oWakeList.write = function() {};
+		oWakeList.remove = function() {};
 
 		return m.render();
 	}
